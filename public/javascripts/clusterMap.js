0fdbcdee18221de81a69e3026d1cc3981bfbe2bb
Olegs <olegs.krivko@gmail.com>
2023-05-10 00:59:24 +0300

    add popup on map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -12,12 +12,20 @@ map.addControl(new tt.NavigationControl());
 var markersOnTheMap = {};
 var eventListenersAdded = false;
 
+console.log(pets);
+
 var points = pets.features.map(function (point, index) {
   return {
     coordinates: [point.longitude, point.latitude],
 
     // properties: { id: index, name: `Point ${index}` },
-    properties: { id: index, name: point.title },
+    properties: {
+      id: index,
+      name: point.title,
+      img: point.images[0].url,
+      petId: point._id,
+      lostDate: point.lostdate,
+    },
   };
 });
 
@@ -167,9 +175,22 @@ function refreshMarkers() {
         console.log(feature.geometry.coordinates);
         newMarker.addTo(map);
         newMarker.setPopup(
-          new tt.Popup({ offset: 30 }).setText(feature.properties.name)
+          // new tt.Popup({ offset: 30 }).setText(feature.properties.name)
+          new tt.Popup({ offset: 10, closeButton: false }).setHTML(
+            `<div style="width: 120px; height: 120px; border-radius: 50%; overflow: hidden;">
+              <a href='/pets/${feature.properties.petId}'>
+                <div style="width: 120px; height: 120px; border-radius: 50%; overflow: hidden;
+                background-image: url(${feature.properties.img});
+                background-size: cover;
+                background-position: center;">
+                </div>
+              </a>
+            </div>`
+          )
         );
+
         markersOnTheMap[id] = newMarker;
+        //newMarker.togglePopup();
       }
     }
   });
@@ -219,6 +240,19 @@ map.on("load", function () {
       "text-color": "white",
     },
   });
+  //my added
+  map.addLayer({
+    id: "unclustered-point",
+    type: "circle",
+    source: "point-source",
+    filter: ["!has", "point_count"],
+    paint: {
+      "circle-color": "#11b4da",
+      "circle-radius": 4,
+      "circle-stroke-width": 1,
+      "circle-stroke-color": "#fff",
+    },
+  });
 
   map.on("data", function (e) {
     if (
@@ -262,3 +296,153 @@ map.on("load", function () {
     map.getCanvas().style.cursor = "";
   });
 });
+
+////////////////
+
+// var map = tt.map({
+//   key: TOMTOMTOKEN,
+//   container: "map",
+//   // style:
+//   //   "https://api.tomtom.com/style/1/style/21.1.0-*?map=basic_night&poi=poi_main",
+//   center: [24.105078, 56.946285],
+//   zoom: 6,
+//   // dragPan: !isMobileOrTablet()
+// });
+// map.addControl(new tt.FullscreenControl());
+// map.addControl(new tt.NavigationControl());
+
+// var points = pets.features.map(function (point, index) {
+//   return {
+//     coordinates: [point.longitude, point.latitude],
+
+//     // properties: { id: index, name: `Point ${index}` },
+//     properties: { id: index, name: point.title },
+//   };
+// });
+// console.log(points);
+
+// var geoJson = {
+//   type: "FeatureCollection",
+//   features: points.map(function (point) {
+//     return {
+//       type: "Feature",
+//       geometry: {
+//         type: "Point",
+//         coordinates: point.coordinates,
+//       },
+//       properties: point.properties,
+//     };
+//   }),
+// };
+
+// // var geoJson = {
+// //   type: "FeatureCollection",
+// //   features: points,
+// // };
+
+// // function getRandomNumberInRange(min, max) {
+// //   return Math.random() * (max - min) + min;
+// // }
+
+// // function generateRandomFeatures() {
+// //   var result = [];
+// //   var numberOfPoints = 0;
+// //   while (numberOfPoints < 1000) {
+// //     result.push({
+// //       type: "Feature",
+// //       geometry: {
+// //         type: "Point",
+// //         coordinates: [
+// //           getRandomNumberInRange(-110, -90),
+// //           getRandomNumberInRange(30, 50),
+// //         ],
+// //       },
+// //     });
+// //     numberOfPoints = numberOfPoints + 1;
+// //   }
+// //   return result;
+// // }
+
+// map.on("load", function () {
+//   map.addSource("randomPoints", {
+//     type: "geojson",
+//     data: geoJson,
+//     cluster: true,
+//     clusterMaxZoom: 14,
+//     clusterRadius: 50,
+//   });
+
+//   map.addLayer({
+//     id: "clusters",
+//     type: "circle",
+//     source: "randomPoints",
+//     filter: ["has", "point_count"],
+//     paint: {
+//       "circle-color": [
+//         "step",
+//         ["get", "point_count"],
+//         "#61ADE0",
+//         10,
+//         "#008D8D",
+//         50,
+//         "#FFDC7A",
+//         100,
+//         "#F9B023",
+//         300,
+//         "#E94743",
+//       ],
+//       "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+//     },
+//   });
+
+//   map.addLayer({
+//     id: "cluster-count",
+//     type: "symbol",
+//     source: "randomPoints",
+//     filter: ["has", "point_count"],
+//     layout: {
+//       "text-field": "{point_count_abbreviated}",
+//       "text-size": 12,
+//     },
+//   });
+
+//   map.addLayer({
+//     id: "unclustered-point",
+//     type: "circle",
+//     source: "randomPoints",
+//     filter: ["!has", "point_count"],
+//     paint: {
+//       "circle-color": "#11b4da",
+//       "circle-radius": 8,
+//       "circle-stroke-width": 1,
+//       "circle-stroke-color": "#fff",
+//     },
+//   });
+
+//   map.on("click", "clusters", function (e) {
+//     var features = map.queryRenderedFeatures(e.point, {
+//       layers: ["clusters"],
+//     });
+//     var clusterId = features[0].properties.cluster_id;
+//     map
+//       .getSource("randomPoints")
+//       .getClusterExpansionZoom(clusterId, function (err, zoom) {
+//         if (err) {
+//           return;
+//         }
+
+//         map.easeTo({
+//           center: features[0].geometry.coordinates,
+//           zoom: zoom,
+//         });
+//       });
+//   });
+
+//   map.on("mouseenter", "clusters", function () {
+//     map.getCanvas().style.cursor = "pointer";
+//   });
+
+//   map.on("mouseleave", "clusters", function () {
+//     map.getCanvas().style.cursor = "";
+//   });
+// });
